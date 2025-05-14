c70e162b5ecc76ed56e2a2cf6f0bdfccff344042
eswaraiah.d <haripriya.attuluru@ISINMAC23H069.local>
2025-05-14 17:50:28 +0530

    Updated the file names and added settings icon in header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../../components/Header/Header'
+import Header from '../../shared/Header/header'
 import Typography from '../../components/Typography/Typography'
 
 export default function Home() {
